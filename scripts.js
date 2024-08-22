067655845c5d112e157873359c8a65d0289ed16a
SodaWIce <ronan.silva2022@hotmail.com>
2024-08-21 21:03:19 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -11,13 +11,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 bookItem.setAttribute('data-genre', livro.genre.toLowerCase());
                 bookItem.innerHTML = `
                     <img src="${livro.image}" alt="${livro.title}" class="book-thumbnail">
-                    <h3 class="book-title" translate="no">${livro.title}</h3>
+                    <h3 class="book-title"translate="no">${livro.title}</h3>
                 `;
 
                 if (isMobile) {
                     bookItem.onclick = () => {
                         showDetails(livro.id);
-                        history.pushState({ page: 'details', bookId: livro.id }, `${livro.title}`, `?book=${livro.id}`);
+                        history.pushState({page: 'details', bookId: livro.id}, `${livro.title}`, `?book=${livro.id}`);
                     };
                 } else {
                     const bookTitle = bookItem.querySelector('.book-title');
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     
                     const handleClick = () => {
                         showDetails(livro.id);
-                        history.pushState({ page: 'details', bookId: livro.id }, `${livro.title}`, `?book=${livro.id}`);
+                        history.pushState({page: 'details', bookId: livro.id}, `${livro.title}`, `?book=${livro.id}`);
                     };
 
                     bookTitle.onclick = handleClick;
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .catch(error => console.error('Erro ao carregar livros:', error));
 
-    window.addEventListener('popstate', function (event) {
+    window.addEventListener('popstate', function(event) {
         if (event.state && event.state.page === 'details') {
             showDetails(event.state.bookId);
         } else {
@@ -45,116 +45,99 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    function showDetails(bookId) {
-        window.scrollTo(0, 0);
-        
-        fetch('books.json')
-            .then(response => response.json())
-            .then(data => {
-                const book = data.books.find(b => b.id === bookId);
-                if (book) {
-                    const mainContent = document.getElementById('mainContent');
-                    const details = document.getElementById('details');
-                    const detailsContent = document.getElementById('detailsContent');
-                    const reportButton = document.getElementById('reportButton');
-                    
-                    mainContent.style.display = 'none';
-                    details.style.display = 'block';
-                    detailsContent.innerHTML = `
-                        <div class="book-header">
-                            <img src="${book.image}" alt="${book.title}" class="book-full">
-                            <a class="download-link" href="${book.pdf}" target="_blank">Baixar PDF</a>
-                        </div>
-                        <div class="book-info">
-                            <p><strong>Título:</strong> ${book.title}</p>
-                            <p><strong>Autor:</strong> ${book.author}</p>
-                            <p><strong>Ano de Publicação:</strong> ${book.year}</p>
-                            <p><strong>Sinopse:</strong> ${book.synopsis}</p>
-                        </div>
-                        <button id="reportButton" class="report-button">Reportar</button>
-                    `;
+function showDetails(bookId) {
+    window.scrollTo(0, 0);
+    
+    fetch('books.json')
+        .then(response => response.json())
+        .then(data => {
+            const book = data.books.find(b => b.id === bookId);
+            if (book) {
+                const mainContent = document.getElementById('mainContent');
+                const details = document.getElementById('details');
+                const detailsContent = document.getElementById('detailsContent');
+    
+                mainContent.style.display = 'none';
+                details.style.display = 'block';
+                detailsContent.innerHTML = `
+                    <div class="book-header">
+                        <img src="${book.image}" alt="${book.title}" class="book-full">
+                        <a class="download-link" href="${book.pdf}" target="_blank">Baixar PDF</a>
+                    </div>
+                    <div class="book-info">
+                        <p><strong>Título:</strong> ${book.title}</p>
+                        <p><strong>Autor:</strong> ${book.author}</p>
+                        <p><strong>Ano de Publicação:</strong> ${book.year}</p>
+                        <p><strong>Sinopse:</strong> ${book.synopsis}</p>
+                    </div>
+                `;
+            }
+        })
+        .catch(error => console.error('Erro ao carregar detalhes do livro:', error));
+}
 
-                    // Atualiza o estado do botão report
-                    updateReportButtonState(bookId);
+function hideDetails() {
+    document.getElementById('mainContent').style.display = 'block';
+    document.getElementById('details').style.display = 'none';
+    history.pushState({page: 'list'}, 'Book List', '?');
+}
 
-                    // Evento para enviar dados quando o reportButton é clicado
-                    reportButton.addEventListener('click', function () {
-                        // Desativa o botão após o envio e muda o texto
-                        if (!reportButton.disabled) {
-                            const formData = new URLSearchParams();
-                            formData.append('entry.1901348521', book.title); // Substitua com o ID do campo do título
+    // Evento para enviar dados quando o reportButton é clicado
+    reportButton.addEventListener('click', function() {
+        // Obtém o conteúdo do `detailsContent`
+        const detailsContent = document.getElementById('detailsContent').textContent;
 
-                            fetch('https://docs.google.com/forms/d/e/1FAIpQLSftSlghH8SQUnueFUlngEXsD_q73G8y2VfIksgJ8Mq8gRG3Vw/formResponse', {
-                                method: 'POST',
-                                body: formData,
-                                mode: 'no-cors'
-                            })
-                                .then(() => {
-                                    reportButton.disabled = true;
-                                    reportButton.textContent = "Avisado!";
-                                    saveReportState(bookId);
-                                })
-                                .catch(error => {
-                                    console.error('Erro ao enviar dados:', error);
-                                });
-                        }
-                    });
-                }
-            })
-            .catch(error => console.error('Erro ao carregar detalhes do livro:', error));
-    }
+        // Procura a linha que contém o título usando "Título:"
+        const titleMatch = detailsContent.match(/Título:\s*(.+)/);
 
-    function hideDetails() {
-        document.getElementById('mainContent').style.display = 'block';
-        document.getElementById('details').style.display = 'none';
-        history.pushState({ page: 'list' }, 'Book List', '?');
-    }
+        // Se encontrar o título, captura o texto correspondente
+        const bookTitle = titleMatch ? titleMatch[1].trim() : 'Título não encontrado';
 
-    function updateReportButtonState(bookId) {
-        const reportButton = document.getElementById('reportButton');
-        const reportedBooks = JSON.parse(localStorage.getItem('reportedBooks')) || [];
+        // Cria um objeto FormData para enviar os dados
+        const formData = new URLSearchParams();
+        formData.append('entry.1901348521', bookTitle); // Substitua com o ID do campo do título
 
-        if (reportedBooks.includes(bookId)) {
+        // Envia os dados para o Google Formulário
+        fetch('https://docs.google.com/forms/d/e/1FAIpQLSftSlghH8SQUnueFUlngEXsD_q73G8y2VfIksgJ8Mq8gRG3Vw/formResponse', {
+            method: 'POST',
+            body: formData,
+            mode: 'no-cors' // Isso pode causar problemas com a visibilidade das respostas enviadas. Se possível, use 'cors'.
+        })
+        .then(response => {
+            console.log('Dados enviados com sucesso.');
+            // Desativa o botão após o envio e muda o texto
             reportButton.disabled = true;
             reportButton.textContent = "Avisado!";
-        } else {
-            reportButton.disabled = false;
-            reportButton.textContent = "Link quebrado?";
-        }
-    }
-
-    function saveReportState(bookId) {
-        let reportedBooks = JSON.parse(localStorage.getItem('reportedBooks')) || [];
-        if (!reportedBooks.includes(bookId)) {
-            reportedBooks.push(bookId);
-            localStorage.setItem('reportedBooks', JSON.stringify(reportedBooks));
-        }
-    }
-
-    function filterBooks() {
-        const input = document.getElementById('searchInput').value.toLowerCase();
-        const genreFilter = document.getElementById('genreFilter').value.toLowerCase();
-        const books = document.getElementById('bookList').getElementsByClassName('book-item');
-
-        Array.from(books).forEach(book => {
-            const title = book.getElementsByClassName('book-title')[0].textContent.toLowerCase();
-            const genre = book.getAttribute('data-genre');
-
-            let showBook = true;
-            if (genreFilter && genreFilter !== 'todos os gêneros' && genre !== genreFilter) {
-                showBook = false;
-            }
-
-            if (input && !title.includes(input)) {
-                showBook = false;
-            }
-
-            book.style.display = showBook ? '' : 'none';
+        })
+        .catch(error => {
+            console.error('Erro ao enviar dados:', error);
         });
-    }
+    });
 
-    function resizeSelectToFit(selectId) {
-        const select = document.getElementById(selectId);
-        const options = Array.from(select.options);
+function filterBooks() {
+    const input = document.getElementById('searchInput').value.toLowerCase();
+    const genreFilter = document.getElementById('genreFilter').value.toLowerCase();
+    const books = document.getElementById('bookList').getElementsByClassName('book-item');
+
+    Array.from(books).forEach(book => {
+        const title = book.getElementsByClassName('book-title')[0].textContent.toLowerCase();
+        const genre = book.getAttribute('data-genre');
+
+        let showBook = true;
+        if (genreFilter && genreFilter !== 'todos os gêneros' && genre !== genreFilter) {
+            showBook = false;
+        }
+
+        if (input && !title.includes(input)) {
+            showBook = false;
+        }
+
+        book.style.display = showBook ? '' : 'none';
+    });
+}
+
+function resizeSelectToFit(selectId) {
+    const select = document.getElementById(selectId);
+    const options = Array.from(select.options);
     }
-});
+}
