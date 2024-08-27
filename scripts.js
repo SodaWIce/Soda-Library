4b289e02133a2e15cc65e610a21d001170988d87
SodaWIce <ronan.silva2022@hotmail.com>
2024-08-27 13:42:40 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -61,17 +61,20 @@ function showDetails(bookId) {
                 mainContent.style.display = 'none';
                 details.style.display = 'block';
                 detailsContent.innerHTML = `
-                    <div class="book-header">
-                        <img src="${book.image}" alt="${book.title}" class="book-full">
-                        <a class="download-link" href="${book.pdf}" target="_blank">Baixar PDF</a>
-                    </div>
-                    <div class="book-info">
-                        <p><strong>Título:</strong> ${book.title}</p>
-                        <p><strong>Autor:</strong> ${book.author}</p>
-                        <p><strong>Ano de Publicação:</strong> ${book.year}</p>
-                        <p><strong>Sinopse:</strong> ${book.synopsis}</p>
-                    </div>
-                `;
+    <div class="book-header">
+        <img src="${book.image}" alt="${book.title}" class="book-full">
+        <a class="download-link" href="${book.pdf}" target="_blank">
+            <img src="https://imgur.com/RFr0SH0.png" alt="Ícone" style="width: 20px; height: 20px; vertical-align: middle; margin-right: 8px;">
+            Baixar PDF
+        </a>
+    </div>
+    <div class="book-info">
+        <p><strong>Título:</strong> ${book.title}</p>
+        <p><strong>Autor:</strong> ${book.author}</p>
+        <p><strong>Ano de Publicação:</strong> ${book.year}</p>
+        <p><strong>Sinopse:</strong> ${book.synopsis}</p>
+    </div>
+`;
             }
         })
         .catch(error => console.error('Erro ao carregar detalhes do livro:', error));
