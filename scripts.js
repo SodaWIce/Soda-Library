f74d9a75cd3672676ec43f72bf7a99e7e17e665b
SodaWIce <ronan.silva2022@hotmail.com>
2024-10-13 14:33:43 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -314,7 +314,7 @@ function showDetails(bookId) {
                         <img src="${book.image}" alt="${book.title}" class="book-full">
                         <a class="download-link" href="${book.pdf}" target="_blank">
                             <img src="https://imgur.com/YZ84GTR.png" alt="Ícone" style="width: 20px; height: 20px; vertical-align: middle; margin-right: 8px;">
-                            Baixar
+                            Download
                         </a>
                     </div>
                     <div class="book-info">
